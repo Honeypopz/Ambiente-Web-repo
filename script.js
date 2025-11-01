fa2d3f7bedf28d0737855a33db5d4bc81d88c324
EValverdeW <e.valverdew@gmail.com>
2025-11-01 12:57:46 -0600

    Script

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("btn");
+
+
+btn.addEventListener("click",()=>{
+    contenedor.classList.toggle("toggle");
+});
+
+const contenedor = document.querySelector(".contenedor");
