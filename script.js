20a5254f3cf239b169f85384c0603afede053dc5
EValverdeW <e.valverdew@gmail.com>
2025-11-10 18:23:36 -0600

    Reserva + js (faltan detalles)

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+  function getDato () {
+    let reserva = document.forms("Reserva");
 
+    let nombre = reserva.nombre.value;
+    let fechaE = document.fechaE.value;
+    let fechaS = reserva.fechaS.value;
+    let adultos = reserva.adultos.value;
+    let ninos = reserva.ninos.value;
+    let hab = reserva.hab.value;
+    let email = reserva.email.value;
 
+   console.log(reserva);
+
+    
+}
 
 
