b540fa6c722128aa320bbcee68ac347225b1529d
EValverdeW <e.valverdew@gmail.com>
2025-11-23 21:06:38 -0600

    Registro de viaje completado, Yupii

--- RegistroViaje.php
+++ RegistroViaje.php
@@ -5,18 +5,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if ($conexion->connect_error) {
     echo '<div class="message error">Error de conexión a la base de datos</div>';
     } else {
-      //datos de solicitud
-      $cantidad = $_POST['cantidad'];
-     $destino = $_POST['destino'];
-     $vehiculo = $_POST['vehiculo'];
-     $ubicacion = $_POST['ubicacion'];
-     
      //buscar conductor por vehiculo
      
+     $sql = ("SELECT cantidad, destino,vehiculo,ubicacion,nombre_conductor FROM viaje v 
+     INNER JOIN conductor c ON v.nombre_conductor = c.nombre_conductor");
+     $viaje= $conexion->query($sql);       
+      while($row = $sql->fetch_array()){
+        echo $row['nombre_conductor']."<br>". $row['conductor']."<br>""<br>";
+      }
     }
 }
   
 ?>
-    
-
-
